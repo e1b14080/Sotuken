98becb619030f3e580a602c185ce368ea92a79a0
MossBall <mossbaaall97531@gmail.com>
2017-12-10 16:46:14 +0900

    振動スクリプトの作成 StrikeVibrationは接触音 OutsideVibrationはなぞり音（こちらはおそらくバグあり）

--- Assets/Working/Scripts/OutsideVibration.cs
+++ Assets/Working/Scripts/OutsideVibration.cs
@@ -1,16 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// 透明な立体物の外側をなぞっているときに音を鳴らす。
+/// 
+/// 使用するためには同オブジェクト内
+/// にAudioSourceのコンポーネントを追加しておくこと。
+/// </summary>
 public class OutsideVibration : MonoBehaviour {
+
+    //なぞり音
+    AudioSource _rubSound;
+    //最新の自身の位置
+    Vector3 _latestPosition;
+    //自身の位置が変化しているか
+    bool _isMoving = false;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        _rubSound = GetComponent<AudioSource>();
+        //最新の位置の初期化
+        _latestPosition = transform.position;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        //透明な立体物の中を移動しているか
+        if(_latestPosition != transform.position)
+        {
+            //移動している場合
+            _isMoving = true;
+            _latestPosition = transform.position;   //最新の位置の更新
+            Debug.Log("isMoving");
+        }else
+        {
+            //移動していない場合
+            _isMoving = false;
+            Debug.Log("isn`t Moving");
+        }
+
+        //透明な立体物の外側に触れているかつ、移動している場合
+        if(other.gameObject.tag != "InsideCollider" && other.gameObject.tag == "OutsideCollider" && _isMoving == true)
+        {
+            if(!_rubSound.isPlaying)
+                _rubSound.Play();
+        }else
+        {
+            _rubSound.Pause();
+        }
+
+    }
+
 }
--- Assets/Working/Scripts/StrikeVibration.cs
+++ Assets/Working/Scripts/StrikeVibration.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 透明な立体物にぶつかったときに一度、接触音を鳴らす。
+/// 
+/// 使用するためには同オブジェクト内
+/// にAudioSourceのコンポーネントを追加しておくこと。
+/// </summary>
+
 public class StrikeVibration : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    //接触音
+    AudioSource _strikeSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        //音の初期化
+        _strikeSound = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //透明なオブジェクトに接触した瞬間に音を鳴らす。
+        if (other.gameObject.tag == "OutsideCollider")
+        {
+            _strikeSound.Play();
+            Debug.Log("Strike");
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
