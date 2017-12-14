42c6d0e0d6921042618118930c4c019721f90d67
oitsmalab <oitsmalab@gmail.com>
2017-12-15 03:23:22 +0900

    手のサイズ・位置調節用スライダーの設置

---
+++ Assets/Script/ScaleSlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleSlider : MonoBehaviour {
+
+	private Slider slider;
+	private float level;
+	GameObject hand;
+
+	void Start () {
+		slider = GetComponent<Slider>();
+		slider.value = 1;
+		hand = GameObject.Find("Hand");
+	}
+
+	void Update (){
+		hand.transform.localScale = new Vector3 (1, slider.value, 1);
+
+	}
+}
