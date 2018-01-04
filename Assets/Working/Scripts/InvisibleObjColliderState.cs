49851c39fceafef9ccf5927bd8450b1b8e40662e
MossBall <mossbaaall97531@gmail.com>
2018-01-04 17:25:40 +0900

    プレイヤーの手が出題用オブジェクトを触れているかを判断、状態を記憶するスクリプト(TouchesInvisibleObjCollider、InvisibleObjColliderState)の作成 次回は外側を触れている時に音を流すスクリプトから状態を記憶するスクリプトにアクセスし、音を鳴らす判断を作成する。

---
+++ Assets/Working/Scripts/InvisibleObjColliderState.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>、
+/// プレイヤーの手が出題用オブジェクトのどのコライダーに触れているかを記憶するクラス。
+/// プロパティを使用して読み書きをする。
+/// こいつはシーン上で唯一無二の存在でなければならない。
+/// </summary>
+
+public class InvisibleObjColliderState : MonoBehaviour {
+    [SerializeField]
+    private bool _touchesOutsideCollider;
+    [SerializeField]
+    private bool _touchesInsideCollider;
+
+    public bool TouchesOutsideCollider
+    {
+        get
+        {
+            return _touchesOutsideCollider;
+        }
+        set
+        {
+            _touchesOutsideCollider = value;
+        }
+    }
+
+    public bool TouchesInsideCollider
+    {
+        get
+        {
+            return _touchesInsideCollider;
+        }
+        set
+        {
+            _touchesInsideCollider = value;
+        }
+    }
+
+}
---
+++ Assets/Working/Scripts/TouchesInvisibleObjCollider.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchesInvisibleObjCollider : MonoBehaviour {
+
+    public enum TouchesCollider { OutsideCollider,InsideCollider };
+
+    public TouchesCollider touchesCollider = TouchesCollider.OutsideCollider;
+    public GameObject ColliderStateManager;
+    private InvisibleObjColliderState _invisibleObjColliderState;
+
+	// Use this for initialization
+	void Start () {
+        _invisibleObjColliderState = ColliderStateManager.GetComponent<InvisibleObjColliderState>();
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Hand")
+        {
+            if (touchesCollider == TouchesCollider.OutsideCollider)
+            {
+                _invisibleObjColliderState.TouchesOutsideCollider = true;
+            }
+
+            if (touchesCollider == TouchesCollider.InsideCollider)
+            {
+                _invisibleObjColliderState.TouchesInsideCollider = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Hand")
+        {
+            if (touchesCollider == TouchesCollider.OutsideCollider)
+            {
+                _invisibleObjColliderState.TouchesOutsideCollider = false;
+            }
+
+            if (touchesCollider == TouchesCollider.InsideCollider)
+            {
+                _invisibleObjColliderState.TouchesInsideCollider = false;
+            }
+        }
+    }
+
+}
